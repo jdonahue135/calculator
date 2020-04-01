068195f64b242819290fc630f90f4c96d581bd68
Jake Donahue <jdonahue135@gmail.com>
2020-04-01 16:21:54 -0400

    implement calculator display functions

--- calculator.js
+++ calculator.js
@@ -12,3 +12,33 @@ function operate(operator, x, y) {
     if (operator == '*') return multiply(x, y);
     if (operator == '/') return divide(x, y);
 }
+
+const numbers = document.querySelectorAll('input.number');
+const display = document.querySelector('div.display');
+
+// set default display value to 0
+var displayValue = 0;
+
+// populate calculator display with numbers
+function populateDisplay() {
+    var displayNumbers = [];
+    
+    //make number buttons work
+    numbers.forEach(number => {
+        number.addEventListener('click', (e) => {
+            displayNumbers.push(number.value);
+            valueNumbers(displayNumbers);
+        })
+    })
+}
+
+//Convert user input to string for display
+function valueNumbers(displayNumbers) {
+    displayValue = displayNumbers[0];
+    for (x = 1; x < displayNumbers.length; x++) {
+        displayValue = displayValue + displayNumbers[x];
+    }
+    display.textContent = displayValue;
+}
+
+populateDisplay();
